810a42224b2346abed7dd66827898bd3b77d6d8b
penszzip <pazhiloyflex@gmail.com>
2023-02-25 16:33:06 -0500

    more views

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -34,5 +34,9 @@ def create_app(test_config=None):
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import bikeshare
+    app.register_blueprint(bikeshare.bp)
+    app.add_url_rule('/', endpoint='index')
     
     return app
